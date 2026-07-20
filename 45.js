23649e8ec033fc28b8d4671e98f5fcd9ef729dcc
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-20 02:24:59 -0700

    first commit

--- 45.js
+++ 45.js
@@ -1,17 +1,31 @@
-// functions
 function singHappyBirthday(){
     console.log("happy birthday to you....");
 }
+
+singHappyBirthday();
+
 function sumThreeNumbers(number1, number2, number3){
     return number1 + number2 + number3;
 }
-function isEven(number){
+
+console.log(sumThreeNumbers(2,3,5));
+
+
+const isEven = function(number){
     return number % 2 === 0;
 }
-function firstChat(anyString){
+
+console.log(isEven(10));
+
+
+function firstChar(anyString){
     return anyString[0];
 }
-function findTarget(array, target){
+
+console.log(firstChar("Ali"));
+
+
+const findTarget = function(array, target){
     for(let i = 0; i < array.length; i++){
         if(array[i] === target){
             return i;
@@ -20,9 +34,4 @@ function findTarget(array, target){
     return -1;
 }
 
-// function calls
-singHappyBirthday(); // 1
-console.log(sumThreeNumbers(2, 3, 5)); // 2
-console.log(isEven(10)); // 3
-console.log(firstChat("Ali")); // 4
-console.log(findTarget([1,2,3,4], 3)); // 5
+console.log(findTarget([1,2,3,4],3));
