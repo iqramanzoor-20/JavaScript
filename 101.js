5436ddf8a685d559420604938d728dbb66771c0e
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-24 18:05:13 -0700

    add 101.js file

---
+++ 101.js
@@ -0,0 +1,8 @@
+
+// select element using query selector
+
+// const mainHeading = document.getElementById("main-heading");
+const mainHeading = document.querySelector("#main-heading");
+const header = document.querySelector(".header");
+const navItem = document.querySelectorAll(".nav-item");
+console.log(navItem);
