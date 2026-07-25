d26be0c6f8575a7ee29dd7e347ead71820094685
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-25 01:49:25 -0700

    first commit

---
+++ 123.js
@@ -0,0 +1,7 @@
+// more event 
+
+const box = document.querySelector(".box");
+
+box.addEventListener("mouseover", () => console.log("mouse over"));
+box.addEventListener("mouseout", () => console.log("mouse out"));
+box.addEventListener("dblclick", () => console.log("double click"));
