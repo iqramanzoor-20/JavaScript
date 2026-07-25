8822eddaf81ff764e0bb8b34092a01b36019e8f2
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-25 01:16:44 -0700

    first commit

---
+++ 114.js
@@ -0,0 +1,13 @@
+// static list vs live list
+
+// querySelectorAll hamein static list degi
+// getElementsBySomething hamein live list degi
+
+const ul = document.querySelector(".todo-list");
+const listItems = ul.getElementsByTagName("li");
+
+const sixthLi = document.createElement("li");
+sixthLi.textContent = "item 6";
+
+ul.append(sixthLi);
+console.log(listItems);
