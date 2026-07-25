c6cbc08de32d445e85e6dcc20fb539236a37a215
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-25 11:55:42 -0700

    first commit

---
+++ 135.js
@@ -0,0 +1,14 @@
+// callback hell 
+setTimeout(()=>{
+    console.log("1");
+
+    setTimeout(()=>{
+        console.log("2");
+
+        setTimeout(()=>{
+            console.log("3");
+        },1000);
+
+    },1000);
+
+},1000);
