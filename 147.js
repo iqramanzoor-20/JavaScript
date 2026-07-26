61925e89f644102b9ae47595bef779875bb368f0
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-07-25 23:48:08 -0700

    fix: clear default todos from localStorage, fix todo form, center list

--- 147.js
+++ 147.js
@@ -1,4 +1,4 @@
 // congratulation 
-
+localStorage.clear(); // purana "new todo" data delete
 console.log("Congratulations! You completed Async JS");
 console.log("Now you know Javascript in Great Detail");
